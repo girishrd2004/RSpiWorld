9561d71c07c404a9199cf49b3e221cbe5f0630af
girishrd2004 <girishrd2004@gmail.com>
2016-07-20 23:03:02 -0700

    Add files via upload

---
+++ App1/src/green_led.js
@@ -0,0 +1,48 @@
+var Gpio = require('onoff').Gpio;
+var led = null;
+var GPIO_PORT = 18;
+var isStarted = false;
+
+var iv = null;
+var ivInternal = null;
+
+exports.onLED = function(){
+	if(!isStarted)
+	{
+	  console.log('export GPIO port : '+ GPIO_PORT);
+	  
+	  led = new Gpio(GPIO_PORT, 'out');
+	  isStarted = true;
+	  
+	  console.log('blink LED');
+	  
+	  var toggleSwitch = false;
+	  iv = setInterval(function(){
+			toggleSwitch = !toggleSwitch;
+			if(toggleSwitch)
+			{
+				ivInternal = setInterval(function(){
+					led.writeSync(led.readSync() ^ 1);
+					}, 500);
+			}else
+			{
+				clearInterval(ivInternal);
+				led.writeSync(0);
+			}
+		},5000);
+	}
+};
+
+exports.offLED =  function(){
+	if(isStarted)
+	{
+	  console.log('release GPIO port :'+ GPIO_PORT);
+	  
+	  clearInterval(iv);
+	  clearInterval(ivInternal);
+	   
+	  led.unexport();
+	  led = null;
+	  isStarted = false;
+	}
+};
