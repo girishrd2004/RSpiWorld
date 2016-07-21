ea8f0df7f19a5e06a555c6a01bcb1657ff3c2e97
girishrd2004 <girishrd2004@gmail.com>
2016-07-20 23:02:33 -0700

    Create index.js

---
+++ App1/src/index.js
@@ -0,0 +1,41 @@
+var mqtt    = require('mqtt');
+var client  = mqtt.connect('mqtt://iot.eclipse.org');
+
+var greenLED = require('./green_led.js');
+var redLED = require('./red_led.js');
+
+var isRedOn = false;
+var isGreenOn = false;
+client.on('connect', function () {
+  client.subscribe('Girish_D');
+});
+ 
+client.on('message', function (topic, message) {
+  
+  var msg = message.toString();
+  console.log(message.toString());
+  if('IamFree' == msg)
+  {
+	if(isRedOn)
+		redLED.offLED();
+	
+	greenLED.onLED();
+	isGreenOn = true;
+  }
+  else if('IamBusy' == msg)
+  {
+	if(isGreenOn)
+		greenLED.offLED();
+	redLED.onLED();
+	isRedOn = true;
+  }
+  else if('ShutDown' == msg)
+  {
+	redLED.offLED();
+	greenLED.offLED();
+	isGreenOn = false;
+	isRedOn = false;
+  }
+});
+
+console.log('waiting for a message');
